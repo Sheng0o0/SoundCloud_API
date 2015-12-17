703a454499d7fb919696cea16b679c6823697c70
Sheng0o0 <shenghaozheng@mybihs.org>
2015-12-17 23:00:59 +0000

    SCP

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-function evaluate(e){
-   //read what is in the input field and give it back to whoever is using 
-}
-
-
-
-
-
-
+SC.initialize({
+          client_id: 'f25be05f9d202816ab3f219f0ca8fdfa'
+        });
+        
 $( document ).ready(function() {    
     $("#searchButton").click(function (evaluate){
        console.log ($("#genreSearch").val());
         SC.get('/tracks', { q: $("#genreSearch").val(), license: 'cc-by-sa' }, function(tracks) {
-          console.log(tracks);
+            for (var i = 0;i < tracks.length; i++ ){
+             $("#result").append("<li>");
+             $("#result").append(tracks[i].title);
+             $("#result").append("<br/>");
+             $("#result").append(tracks[i].genre);
+             $("#result").append("<br/>");
+              $("#result").append(tracks[i].permalink_url);
+             $("#result").append("</li>");
+             
+            }
         });
     })
     
